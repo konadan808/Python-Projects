debdc637ff5f12e082c52ee85548cc2cc99fb8e4
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-10-03 14:53:50 -0700

    Commit of Comments
    Committing add of comments to Polymorphism assignment

--- Polymorphism Submission Assignment/Polymorphism Submission Assignment.py
+++ Polymorphism Submission Assignment/Polymorphism Submission Assignment.py
@@ -1,6 +1,6 @@
 # Polymorphism Submission Assignment
 
-
+# Parent class of Droid
 class Droid:
     def __init__(self, name, function, owner, output):
         self.name = name
@@ -12,11 +12,10 @@ class Droid:
         specs = "\nName: {}\nFunction: {}\nOwner: {}\nCommunication Method: {}".format(self.name,self.function,self.owner,self.output)
         return specs
 
-
     def audioOutput(self):
         msg = "Input Droid speech here"
         return msg
-
+# Child class of protocolDroid
 class protocolDroid(Droid):
     def __init__(self, name, function, owner, output, color, language):
         
@@ -31,11 +30,10 @@ class protocolDroid(Droid):
         specs = "\nName: {}\nFunction: {}\nOwner: {}\nCommunication Method: {}\nColor: {}\nLanguages Spoken {}".format(self.name,self.function,self.owner,self.output,self.color,self.language)
         return specs
 
-
     def audioOutput(self):
         msg = "\"I am C-3P0, human cyborg relations.\""
         return msg
-
+# Child class of astromech
 class astromech(Droid):
     def __init__(self, name, function, owner, output, skills, gadgets):
         
@@ -50,17 +48,17 @@ class astromech(Droid):
         specs = "\nName: {}\nFunction: {}\nOwner: {}\nCommunication Method: {}\nSkills: {}\nGadgets {}".format(self.name,self.function,self.owner,self.output,self.skills,self.gadgets)
         return specs
 
-
     def audioOutput(self):
         msg = "\"Beeb beeb boop, beep!.\""
         return msg
 
 
-
 if __name__ == "__main__":
+    
     droid1 = protocolDroid("C-3P0", "Human Relations", "Princess Leia", "Spoken", "Gold", "Over 3 million forms of communication")
     print(droid1.information())
     print(droid1.audioOutput())
+
     droid2 = astromech("R2-D2", "Astromech", "Luke Skywalker", "Binary, beeps, and whisltes", "Computer interfacing, piloting, hero extraordinaire", "Circular saw, taser, computer interface, jets, comlink")
     print(droid2.information())
     print(droid2.audioOutput())
